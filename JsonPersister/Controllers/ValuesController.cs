0711821aceea6a181644f072b2e08eda418d5c6f
Renan Stigliani <malke.eklam@gmail.com>
2015-04-20 14:24:47 -0300

    Changing Memcache to REDIS

--- JsonPersister/Controllers/ValuesController.cs
+++ JsonPersister/Controllers/ValuesController.cs
@@ -1,56 +1,136 @@
-﻿using Enyim.Caching;
-using Enyim.Caching.Memcached;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
-
+using System.Configuration;
 using System.Linq;
-
 using System.Web.Http;
 
 namespace JsonPersister.Controllers
 {
     public class ValuesController : ApiController
     {
-        private static Lazy<MemcachedClient> cache = new Lazy<MemcachedClient>(() => new MemcachedClient());
-
-        private static MemcachedClient Cache
+        public IEnumerable<Resource> Get(string appId)
         {
-            get
+            return new ResourceRepository(appId).ListAll().ToList();
+        }
+
+        public JObject Get(string appId, string resourceId)
+        {
+            return new ResourceRepository(appId).GetById(resourceId).Content;
+        }
+
+        public string Post(string appId, [FromBody]JObject resource)
+        {
+            return new ResourceRepository(appId).Add(resource);
+        }
+
+        public void Put(string appId, string resourceId, [FromBody]JObject resource)
+        {
+            new ResourceRepository(appId).Add(resourceId, resource);
+        }
+
+        public void Delete(string appId, string resourceId = null)
+        {
+            if (resourceId == null)
             {
-                return cache.Value;
+                new ResourceRepository(appId).DeleteAll();
+            }
+            else
+            {
+                new ResourceRepository(appId).Delete(resourceId);
             }
         }
 
-        public IEnumerable<dynamic> Get(string appId)
+        [Serializable]
+        public struct Resource
         {
-            return new string[] { "value1", "value2" };
+            public Resource(string id, JObject content)
+            {
+                this.Id = id;
+                this.Content = content;
+            }
+
+            public string Id;
+
+            public JObject Content;
         }
 
-        public dynamic Get(string appId, string resourceId)
+        public class ResourceRepository
         {
-            var id = GetId(appId, resourceId);
+            private readonly string appId;
 
-            return Cache.Get(id);
-        }
+            public ResourceRepository(string appId)
+            {
+                this.appId = appId;
+            }
 
-        public void Post([FromBody]dynamic value)
-        {
-        }
+            private static ConnectionMultiplexer redis;
 
-        public void Put(string appId, string resourceId, [FromBody]dynamic resource)
-        {
-            var id = GetId(appId, resourceId);
+            private static ConnectionMultiplexer Redis
+            {
+                get
+                {
+                    if (redis == null)
+                    {
+                        var redisCloudUrl = ConfigurationManager.AppSettings["REDISCLOUD_URL"].ToString();
+                        var connectionUri = new Uri(redisCloudUrl);
+                        string host = connectionUri.Host;
+                        int port = connectionUri.Port;
+                        string password = connectionUri.UserInfo.Split(':').LastOrDefault();
 
-            Cache.Store(StoreMode.Set, id, resource);
-        }
+                        ConfigurationOptions options = new ConfigurationOptions();
+                        options.Password = password;
+                        options.EndPoints.Add(host, port);
 
-        public void Delete(string id)
-        {
-        }
+                        redis = ConnectionMultiplexer.Connect(options);
+                    }
+                    return redis;
+                }
+            }
 
-        private static string GetId(string appId, string id)
-        {
-            return string.Format("{0}/{1}", appId, id);
+            public IEnumerable<Resource> ListAll()
+            {
+                var db = Redis.GetDatabase();
+
+                var list = db.ListRange(appId, 0, -1);
+
+                return list.Select(x => JsonConvert.DeserializeObject<Resource>(x));
+            }
+
+            public Resource GetById(string id)
+            {
+                return ListAll().SingleOrDefault(x => x.Id == id);
+            }
+
+            public void Add(string id, JObject content)
+            {
+                var db = Redis.GetDatabase();
+
+                db.ListLeftPush(appId, JsonConvert.SerializeObject(new Resource(id, content)));
+            }
+
+            public string Add(JObject content)
+            {
+                var id = Guid.NewGuid().ToString();
+                Add(id, content);
+                return id;
+            }
+
+            public void DeleteAll()
+            {
+                var db = Redis.GetDatabase();
+
+                db.KeyDelete(appId);
+            }
+
+            public void Delete(string id)
+            {
+                var db = Redis.GetDatabase();
+
+                db.ListRemove(appId, JsonConvert.SerializeObject(GetById(id)), -1);
+            }
         }
     }
 }
