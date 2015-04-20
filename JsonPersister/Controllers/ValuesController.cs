7c9fd977042518e505b7ab7fa5302286d60642c6
Renan Stigliani <malke.eklam@gmail.com>
2015-04-20 09:30:58 -0300

    Adding Enyim.Caching.Memcached

--- JsonPersister/Controllers/ValuesController.cs
+++ JsonPersister/Controllers/ValuesController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using Enyim.Caching;
+using Enyim.Caching.Memcached;
+using System;
 using System.Collections.Generic;
+
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web;
+
 using System.Web.Http;
 
 namespace JsonPersister.Controllers
 {
     public class ValuesController : ApiController
     {
-        public IEnumerable<dynamic> Get()
+        private static Lazy<MemcachedClient> cache = new Lazy<MemcachedClient>(() => new MemcachedClient());
+
+        private static MemcachedClient Cache
+        {
+            get
+            {
+                return cache.Value;
+            }
+        }
+
+        public IEnumerable<dynamic> Get(string appId)
         {
             return new string[] { "value1", "value2" };
         }
@@ -18,18 +29,19 @@ namespace JsonPersister.Controllers
         public dynamic Get(string appId, string resourceId)
         {
             var id = GetId(appId, resourceId);
-            return Cache[id];
+
+            return Cache.Get(id);
         }
 
         public void Post([FromBody]dynamic value)
         {
-
         }
 
         public void Put(string appId, string resourceId, [FromBody]dynamic resource)
         {
             var id = GetId(appId, resourceId);
-            Cache[id] = resource;
+
+            Cache.Store(StoreMode.Set, id, resource);
         }
 
         public void Delete(string id)
@@ -39,14 +51,6 @@ namespace JsonPersister.Controllers
         private static string GetId(string appId, string id)
         {
             return string.Format("{0}/{1}", appId, id);
-        }
-
-        private System.Web.Caching.Cache Cache
-        {
-            get
-            {
-                return HttpContext.Current.Cache;
-            }
         }
     }
 }
